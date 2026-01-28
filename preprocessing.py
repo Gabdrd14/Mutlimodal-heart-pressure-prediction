1a774366de1ec86d30a08addab3093a1a56fa10f
gabriel <gabriel@MacBook-Pro-de-gabriel.local>
2026-01-28 11:09:07 +0100

    ajout loader raw file format .mat

--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.signal import butter, filtfilt, medfilt
 from scipy.interpolate import interp1d
 from scipy.signal import lfilter
-
+from scipy.io import io
 import matplotlib.pyplot as plt
 import pywt
 
@@ -11,7 +11,9 @@ import pywt
 # 1. DATA LOADER
 # ==============================
 
-class RHCDataLoader:
+class DataLoader_preprocess_file:
+
+
     def __init__(self, record_path):
         self.record_path = record_path
 
@@ -20,7 +22,24 @@ class RHCDataLoader:
         signals = record.p_signal
         names = record.sig_name
         return dict(zip(names, signals.T))
+    
 
+
+class  Dataloader_raw_file:
+    def __init__(self):
+        self.record_path = record_path
+
+
+    def load(self):
+        mat = scipy.io.loadmat(record_path)
+        data = mat['data'][0,0]
+        ecg = data['E_data'].squeeze()
+        t   = data['E_time'].squeeze()
+
+        # Sécurité
+        N = min(len(ecg), len(t))
+        ecg = ecg[:N]
+        t   = t[:N]
 
 # ==============================
 # 2. ARTIFACT REMOVAL
@@ -66,7 +85,7 @@ class ArtifactCleaner:
 
 
 
-    def swt_filter(self,sig,wavelet = "db4",level = 4 , method = 'soft'):
+    def swt_filter(self,sig,wavelet = "db4",level = 2 , method = 'soft'):
 
         """
         Docstring pour swt_filter
@@ -80,6 +99,8 @@ class ArtifactCleaner:
         :param method: seuillage des coefficients
 
         """
+        if len(sig) % 2 != 0:
+            sig = sig[:-1]  # ou np.append(sig, sig[-1]) pour ne rien perdre
 
         coeffs= pywt.swt(sig,wavelet,level=level)
         threshold = lambda c , sigma: pywt.threshold(c, sigma * np.sqrt(2*np.log(len(c))), mode= method)
@@ -164,7 +185,7 @@ class ArtifactCleaner:
 
 class CleanPreprocessingPipeline:
     def __init__(self, record_path):
-        self.loader = RHCDataLoader(record_path)
+        self.loader = DataLoader(record_path)
         self.cleaner = ArtifactCleaner()
 
     def run(self):
@@ -178,21 +199,26 @@ class CleanPreprocessingPipeline:
             data["patch_ACC_dv"]
         ) / 3
 
-        # ========== ECG cleaning ==========
-        ecg_clean = self.cleaner.highpass(ecg_raw)
-        ecg_clean = self.cleaner.bandpass(ecg_raw, 1 , 40)
-        ecg_clean = self.cleaner.suppress_motion(ecg_clean)
-        ecg_clean = self.cleaner.hampel_filter(ecg_clean)
+        # # ========== ECG cleaning ==========
+        # ecg_clean = self.cleaner.highpass(ecg_raw)
+        # ecg_clean = self.cleaner.suppress_motion(ecg_clean)
+        # ecg_clean = self.cleaner.hampel_filter(ecg_clean)
+
+        ecg_clean = ecg_raw
+
 
         # ========== SCG cleaning ==========
 
-        scg_clean = self.cleaner.bandpass(scg_raw, 1, 40)
-        scg_clean = self.cleaner.suppress_motion(scg_clean)
-        scg_clean = self.cleaner.hampel_filter(scg_clean)
+        # scg_clean = self.cleaner.bandpass(scg_raw, 1, 40)
+        # scg_clean = self.cleaner.suppress_motion(scg_clean)
+        # scg_clean = self.cleaner.hampel_filter(scg_clean)
+        # scg_clean = self.cleaner.swt_filter(scg_clean)
+
+        scg_clean = scg_raw
 
         # ========== Normalize ==========
         ecg_clean = (ecg_clean - np.mean(ecg_clean)) / np.std(ecg_clean)
-        scg_clean = (scg_clean - np.mean(scg_clean)) / np.std(scg_clean)
+        # scg_clean = (scg_clean - np.mean(scg_clean)) / np.std(scg_clean)
 
 
         return {
@@ -207,11 +233,12 @@ class CleanPreprocessingPipeline:
 
 
 
-def plot_ecg_scg(ecg_raw, ecg_clean, scg_raw, scg_clean, fs=500, start_time=0):
+
+def plot_ecg_scg(ecg_raw, ecg_clean, scg_raw, scg_clean, fs=500, start_time=20):
 
 
     start_idx = int(start_time * fs)
-    end_idx = start_idx   + 40    * fs  # 20-second window
+    end_idx = start_idx   + 10   * fs  # 20-second window
 
     # Slice signals
     ecg_raw_win = ecg_raw[start_idx:end_idx]
@@ -249,7 +276,7 @@ def plot_ecg_scg(ecg_raw, ecg_clean, scg_raw, scg_clean, fs=500, start_time=0):
 # ==============================
 
 
-record_path = "processed_data/TRM137-RHC1/TRM137-RHC1"
+record_path = "1.0.0/processed_data/TRM107-RHC1"
 
 record = wfdb.rdrecord(record_path)
 print(record.sig_name)
